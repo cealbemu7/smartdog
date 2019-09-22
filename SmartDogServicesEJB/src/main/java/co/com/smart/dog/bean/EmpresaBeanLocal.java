77b1c15218395c12899c2ae8ce02a05258594376
Andres <aaburgos9@misena.edu.co>
2019-09-21 20:34:13 -0500

    servicios

---
+++ SmartDogServicesEJB/src/main/java/co/com/smart/dog/bean/EmpresaBeanLocal.java
@@ -0,0 +1,13 @@
+package co.com.smart.dog.bean;
+
+import java.util.List;
+
+import co.com.smart.dog.infraestructure.dto.EmpresaDTO;
+import co.com.smart.dog.infraestructure.dto.SmartExcepcionSerializada;
+
+public interface EmpresaBeanLocal {
+	
+	public List<EmpresaDTO> consultarEmpresa (EmpresaDTO empresa ) throws SmartExcepcionSerializada;
+	public EmpresaDTO grabarEmpresa(EmpresaDTO empresa ) throws SmartExcepcionSerializada;
+	
+}

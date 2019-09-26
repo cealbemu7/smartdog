1309f8ecd49bf24ffef3537985da461e1a873356
ceramosh <carlosramoshoyos@gmail.com>
2019-09-26 18:33:17 -0500

    Terminar metodo grabar empresa servicio empresas
    Terminar metodo grabar empresa servicio empresas

--- SmartDogServicesEJB/src/main/java/co/com/smart/dog/persistence/caller/EmpresaCaller.java
+++ SmartDogServicesEJB/src/main/java/co/com/smart/dog/persistence/caller/EmpresaCaller.java
@@ -2,15 +2,17 @@ package co.com.smart.dog.persistence.caller;
 
 import java.io.IOException;
 import java.io.Serializable;
+import java.math.BigDecimal;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
 import javax.naming.NamingException;
+
 import co.com.smart.dog.infraestructure.dto.CiudadDTO;
 import co.com.smart.dog.infraestructure.dto.DepartamentoDTO;
-
 import co.com.smart.dog.infraestructure.dto.EmpresaDTO;
+import co.com.smart.dog.infraestructure.dto.MensajeSQLDTO;
 import co.com.smart.dog.persistence.entity.util.JDBCResourceManager;
 
 public class EmpresaCaller extends JDBCResourceManager implements Serializable {
@@ -34,21 +36,31 @@ public class EmpresaCaller extends JDBCResourceManager implements Serializable {
 		try { 
 			conn = getConnection();
 			call = conn.prepareCall(getString("EmpresaCaller.fn_grabar_empresa"));
-			if (empresa.getScempresa() != null) {
-                call.setBigDecimal(1, empresa.getScempresa());
-            } else {
-                call.setNull(1, java.sql.Types.NULL);
-            }
-				call.setBigDecimal(2, empresa.getScempresa());
-				call.setString(3, empresa.getNitempresa());
-				call.setString(4, empresa.getDsrazonsocial());
-				call.setString(5, empresa.getDsemail());
-				call.setString(6, empresa.getDstelefono());
-				call.setString(7, empresa.getCousuario());
-				call.setBigDecimal(8, empresa.getCiudad().getScciudad());
-				call.setBigDecimal(9, empresa.getCiudad().getDepartamento().getScdepartamento());
-				
 			
+			if (empresa.getScempresa() != null) {
+				call.setInt(1, empresa.getScempresa().intValue());
+            } else {
+                call.setNull(1, java.sql.Types.INTEGER);
+            }
+				
+			call.setString(2, empresa.getNitempresa());
+			call.setString(3, empresa.getDsrazonsocial());
+			call.setString(4, empresa.getDsemail());
+			call.setString(5, empresa.getDstelefono());
+			call.setString(6, empresa.getCousuario());
+			call.setInt(7, empresa.getCiudad().getScciudad().intValue());
+			call.setInt(8, empresa.getCiudad().getDepartamento().getScdepartamento().intValue());		
+			call.setString(9, empresa.getDsdireccion());
+			
+			call.registerOutParameter(10, java.sql.Types.INTEGER);
+			call.registerOutParameter(11, java.sql.Types.VARCHAR);
+			
+			call.executeUpdate();
+			
+			empresa.setScempresa(new BigDecimal(call.getInt(10))); 
+			MensajeSQLDTO msj = getResponseSQL(call.getString(11));
+			empresa.setCodigo(msj.getCodigo());
+			empresa.setDescripcion(msj.getDescripcion());
 		}finally {
 			closeResources(conn, call);
 		}
@@ -94,5 +106,17 @@ public class EmpresaCaller extends JDBCResourceManager implements Serializable {
 		return empresalist;
 	}
 
+	
+	public MensajeSQLDTO getResponseSQL(String response) {
+		MensajeSQLDTO sqldto = new MensajeSQLDTO();
+		
+		if(response != null){
+			String[] msj = response.split(":");
+			sqldto.setCodigo(msj[0]);
+			sqldto.setDescripcion(msj[1]);
+		}
+		
+		return sqldto;
+	}
 
 }

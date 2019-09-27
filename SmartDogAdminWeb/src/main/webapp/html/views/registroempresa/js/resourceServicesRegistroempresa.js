28d088342fed3b6b16c8ccd9c47dfef0ec9a11a4
Andres <aaburgos9@misena.edu.co>
2019-09-27 10:12:45 -0500

    cambios controlador vista registro de empresa
    cambios controlador vista registro de empresa

---
+++ SmartDogAdminWeb/src/main/webapp/html/views/registroempresa/js/resourceServicesRegistroempresa.js
@@ -0,0 +1,10 @@
+/**
+ * @Descripcion: Definicion de los servicios a consumir para el modulo de empresas.
+ * @Author: Andres Brgs.
+ * @Date: 26-09-2019
+ * @Date Modifi: 26-09-2019
+ */
+var serviceNameEmpresa = '/SmartServiceEmpresa';
+
+var methodConsultarEmpresa  = serviceNameEmpresa+'/consultarEmpresa';
+var methodGrabarEmpresa  = serviceNameEmpresa+'/grabarEmpresa';

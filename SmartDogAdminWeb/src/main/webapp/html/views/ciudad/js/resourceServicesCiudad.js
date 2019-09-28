33642b465e4645928c16ba79f6cf1273af99d82c
aaburgos9 <aaburgos9@misena.edu.co>
2019-09-28 11:14:44 -0500

    actulizacion de controller

---
+++ SmartDogAdminWeb/src/main/webapp/html/views/ciudad/js/resourceServicesCiudad.js
@@ -0,0 +1,9 @@
+/**
+ * @Descripcion:
+ * @Author: Andres Brgs.
+ * @Date: 26-09-2019
+ * @Date Modificado: 26-09-2019
+ */
+var SmartServiceCiudad = '/SmartServiceCiudad';
+
+var methodListarCiudad  = SmartServiceCiudad+'/listarCiudad';
---
+++ SmartDogAdminWeb/src/main/webapp/html/views/departamento/js/resourceServicesDepartamento.js
@@ -0,0 +1,9 @@
+/**
+ * @Descripcion: 
+ * @Author: Andres Brgs.
+ * @Date: 26-09-2019
+ * @Date Modificado: 26-09-2019
+ */
+var SmartServiceDepartamento = '/SmartServiceDepartamento';
+
+var methodListarDepartamento  = SmartServiceDepartamento+'/listarDepartamento';

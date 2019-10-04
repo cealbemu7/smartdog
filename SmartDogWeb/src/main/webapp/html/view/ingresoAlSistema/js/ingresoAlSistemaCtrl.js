334837c98c5669ff3eb6b34f809d45bd9ef63214
Deymer <ddhoyos0@misena.edu.co>
2019-10-03 20:18:00 -0500

    maquetacion del reserver cita
    creacion de html del modulo re reservar cita, terceros y consultar cita

--- SmartDogWeb/src/main/webapp/html/view/ingresoAlSistema/js/ingresoAlSistemaCtrl.js
+++ SmartDogWeb/src/main/webapp/html/view/ingresoAlSistema/js/ingresoAlSistemaCtrl.js
@@ -45,6 +45,7 @@ angular.module('smartApp').controller('ingresoAlSistemaCtrl',function($scope, sm
 								
 								alert("el usuario existe con este correo");
 								$scope.ShowForm();
+								$scope.formVisible = false;
 							}else{
 								alert("No se encontro usuario con este correo");
 							}
@@ -73,9 +74,53 @@ angular.module('smartApp').controller('ingresoAlSistemaCtrl',function($scope, sm
 			 alert("Error", "Ha ocurrido un error al momento de consultar usuario", error.message);
 		  }
 	 }
+	$scope.formVisible = true;
 	$scope.formVisibility = false;
-	$scope.ShowForm = function(){
+	$scope.ShowForm = function(){		
 		$scope.formVisibility = true;
+		
 	}
 	 
+});
+$(document).ready(function () {
+
+    var navListItems = $('div.setup-panel div a'),
+        allWells = $('.setup-content'),
+        allNextBtn = $('.nextBtn');
+
+    allWells.hide();
+
+    navListItems.click(function (e) {
+        e.preventDefault();
+        var $target = $($(this).attr('href')),
+            $item = $(this);
+
+        if (!$item.hasClass('disabled')) {
+            navListItems.removeClass('btn-success').addClass('btn-default');
+            $item.addClass('btn-success');
+            allWells.hide();
+            $target.show();
+            $target.find('input:eq(0)').focus();
+        }
+    });
+
+    allNextBtn.click(function () {
+        var curStep = $(this).closest(".setup-content"),
+            curStepBtn = curStep.attr("id"),
+            nextStepWizard = $('div.setup-panel div a[href="#' + curStepBtn + '"]').parent().next().children("a"),
+            curInputs = curStep.find("input[type='text'],input[type='url']"),
+            isValid = true;
+
+        $(".form-group").removeClass("has-error");
+        for (var i = 0; i < curInputs.length; i++) {
+            if (!curInputs[i].validity.valid) {
+                isValid = false;
+                $(curInputs[i]).closest(".form-group").addClass("has-error");
+            }
+        }
+
+        if (isValid) nextStepWizard.removeAttr('disabled').trigger('click');
+    });
+
+    $('div.setup-panel div a.btn-success').trigger('click');
 });
